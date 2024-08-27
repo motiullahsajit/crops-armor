ca47fc26dd90376b906485e24bf54303daf422c7
Motiullah Sajit <sajitmotiullah@gmail.com>
2024-08-27 20:52:34 +0600

    arduino code added

--- crops-armor-server/src/utils/features.ts
+++ crops-armor-server/src/utils/features.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = (uri: string) => {
   mongoose
     .connect(uri, {
-      dbName: "swift_cart",
+      dbName: "cropsarmor",
     })
     .then((c) => console.log(`DB Connected to ${c.connection.host}`))
     .catch((e) => console.log("DB connection error: ", e));
